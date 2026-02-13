b37306fb15a9b7d21d7b2de1deea809ad8e6d278
Angus Hollands <goosey15@gmail.com>
2026-02-13 12:50:09 +0000

    fix: update flakes

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs"; # keep nixvim nixpkgs consistent with nixpkgs
-    };
     mystmd = {
       url = "git+https://gist.github.com/73cf022e5d9f8ce99d8e9d5bff10e5d9";
       inputs.nixpkgs.follows = "nixpkgs"; # keep nixvim nixpkgs consistent with nixpkgs
@@ -15,17 +11,21 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     mystmd,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      packages = [mystmd.packages.${system}.myst];
-    in {
-      devShell = pkgs.mkShell {
-        inherit packages;
-      };
-    });
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        packages = [mystmd.packages.${system}.myst];
+      in {
+        default = pkgs.mkShell {
+          inherit packages;
+        };
+      }
+    );
+  };
 }
